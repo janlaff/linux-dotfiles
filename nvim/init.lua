4dd1fd544ec3274ae8b8cf03363e0c7d0483da94
Jan Lafferton <jan@lafferton.de>
2023-05-18 18:07:25 +0200

    Bootstrapped neovim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,12 +5,19 @@ vim.opt.tabstop = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.cursorline = true
-
-vim.opt.termguicolors = true
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+vim.opt.termguicolors = true
 vim.keymap.set("i", "jk", "<ESC>")
-
+vim.keymap.set("n", "<Tab>", ":bn<CR>")
+vim.keymap.set("n", "<C-Tab>", ":bp<CR>")
+vim.keymap.set('n', '<leader>ff', ":Telescope find_files<CR>")
+vim.keymap.set('n', '<leader>fg', ":Telescope live_grep<CR>")
+vim.keymap.set('n', '<leader>fb', ":Telescope buffers<CR>")
+vim.keymap.set('n', '<leader>fh', ":Telescope help_tags<CR>")
+vim.keymap.set('n', '<leader>fo', ":Telescope oldfiles<CR>")
+vim.keymap.set("n", "<leader>nn", ":Neorg keybind norg core.dirman.new.note<CR>")
+vim.keymap.set("n", "<leader>nt", ":Neorg tangle current-file<CR>")
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -23,91 +30,73 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
-  {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
-    config = true,
-  },
-  {
-    "folke/which-key.nvim",
-    config = true,
-  },
-  {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.1',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-      vim.keymap.set('n', '<leader>fo', builtin.oldfiles, {})
-    end
-  },
-  {
-    "folke/tokyonight.nvim",
-    config = function()
-      vim.cmd.colorscheme "tokyonight-storm"
-    end,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    main = "nvim-treesitter.configs",
-    opts = {
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
-      sync_install = false,
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      }
+local plugins = {
+{
+  "kylechui/nvim-surround",
+  version = "*", -- Use for stability; omit to use `main` branch for the latest features
+  event = "VeryLazy",
+  config = true,
+},
+{
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.1',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+},
+{   
+  "folke/tokyonight.nvim",
+  config = function()
+    vim.cmd.colorscheme("tokyonight-storm")
+  end,
+},
+{
+  'nvim-lualine/lualine.nvim',
+  -- Also use tokyonight theme here :)
+  opts = { options = { theme = "tokyonight" } },
+  dependencies = { 'nvim-tree/nvim-web-devicons' }
+},
+{
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  main = "nvim-treesitter.configs",
+  opts = {
+    ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
     }
-  },
-  {
-    "nvim-neorg/neorg",
-    build = ":Neorg sync-parsers",
-    config = function() 
-      require("neorg").setup({
-        load = {
-          ["core.defaults"] = {}, -- Loads default behaviour
-          ["core.concealer"] = {}, -- Adds pretty icons to your documents
-          ["core.tangle"] = {},
-          ["core.esupports.metagen"] = {
-            config = {
-              type = "auto",
-            }
-          },
-          ["core.dirman"] = { -- Manages Neorg workspaces
-          config = {
-            workspaces = {
-              dotfiles = "~/linux-dotfiles"
-            },
+  }
+},
+{   
+  "nvim-neorg/neorg",
+  build = ":Neorg sync-parsers",
+  opts = {
+    load = {
+      ["core.defaults"] = {}, -- Loads default behaviour
+      ["core.concealer"] = {}, -- Adds pretty icons to your documents
+      ["core.tangle"] = {}, -- Add tangling support
+      ["core.esupports.metagen"] = { -- Automatically sync document meta
+        config = {
+          type = "auto",
+        }
+      },
+      ["core.dirman"] = { -- Manages Neorg workspaces
+        config = {
+          workspaces = {
+            dotfiles = "~/linux-dotfiles"
           },
         },
       },
-    })
-
-    vim.keymap.set("n", "<leader>nn", ":Neorg keybind norg core.dirman.new.note<CR>")
-    vim.keymap.set("n", "<leader>nt", ":Neorg tangle current-file<CR>")
-    -- vim.keymap.set("n", "<leader>ni", ":Neorg inject-metadata<CR>")
-
-    --vim.api.nvim_create_autocmd("BufWritePre", {
-    --  pattern = { "*.norg" },
-    --  command = ":Neorg update-metadata",
-    --})
-  end,
+    },
+  },
   dependencies = { 
     "nvim-lua/plenary.nvim"
   },
 },
 {
-  'nvim-lualine/lualine.nvim',
-  opts = { options = { theme = "tokyonight" } },
-  dependencies = { 'nvim-tree/nvim-web-devicons' }
+  "folke/which-key.nvim",
+  config = true,
+},
 }
-})
+require("lazy").setup(plugins)
