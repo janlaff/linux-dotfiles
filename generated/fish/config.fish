0b6513570dc3d52d364b2982b12586ec0c384886
Jan Lafferton <jan@lafferton.de>
2023-05-18 22:44:13 +0200

    Default editor

--- generated/fish/config.fish
+++ generated/fish/config.fish
@@ -15,7 +15,6 @@ set -l purple 9d7cd8
 set -l cyan 7dcfff
 set -l pink bb9af7
 
-
 # Syntax Highlighting Colors
 set -g fish_color_normal $foreground
 set -g fish_color_command $cyan
@@ -35,9 +34,9 @@ set -g fish_color_escape $pink
 set -g fish_color_autosuggestion $comment
 
 # Completion Pager Colors
-
 set -g fish_pager_color_progress $comment
 set -g fish_pager_color_prefix $cyan
 set -g fish_pager_color_completion $foreground
 set -g fish_pager_color_description $comment
 set -g fish_pager_color_selected_background --background=$selection
+set -gx EDITOR nvim
